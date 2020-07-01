e9dcbef019bf23940f080f8a0f3d0ad311d50179
Danilqa <hello.boriskas@gmail.com>
2020-07-02 00:07:05 +0300

    navigation-animation: refactored code

--- source/js/modules/navigation-animation.js
+++ source/js/modules/navigation-animation.js
@@ -8,21 +8,19 @@ export default () => {
 
   const toMenuItemName = (ref) => ref.replace(`#`, ``);
 
-  const animationWithLinks = document.querySelectorAll(`.with-animation-before`);
-  const body = document.querySelector(`body`);
-
   let activeTimeout;
-
+  const body = document.querySelector(`body`);
+  const animationWithLinks = document.querySelectorAll(`.with-animation-before`);
   animationWithLinks.forEach((link) => {
     link.addEventListener(`click`, (event) => {
-      const from = toMenuItemName(window.location.hash);
-      clearTimeout(activeTimeout);
-
       event.preventDefault();
-      const to = toMenuItemName(link.hash);
 
       body.classList.add(`transition-active`);
 
+      clearTimeout(activeTimeout);
+
+      const from = toMenuItemName(window.location.hash);
+      const to = toMenuItemName(link.hash);
       const timeout = (ROUTE_NAVIGATION_DELAY[from] || {})[to] || 0;
       activeTimeout = setTimeout(() => {
         document.location = link.href;
